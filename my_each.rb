5b8c785aec0f61c644735c5e05ab3f75af3cc742
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 22:20:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(arr)
   # code here
+  count = 0
+  while count < arr.size
+  block_given?
+  yield(arr[count])
+  count += 1
 end
+arr  
+end
+
+
+
+
+  
+  
